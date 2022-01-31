2fbfa8bfba371e13e812db47fd9551e43bd29bc9
Brindha6384 <brindhamurugan49@gmail.com>
2022-01-31 19:32:45 +0530

    ping pong

---
+++ Article.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Article = ()=>
+
+{
+    return<div>
+
+<b><h1>BIRIYANI</h1></b>
+
+<p>The exact origin of the dish is uncertain. In north india, different varities of biriyani developed in the muslim centres of delhi,rampur,licknow ans other small principalities. In south india, where rice is more widely used as a staple food. several distinct varities of biriyani emerged from hydrabad deccan some beleive the dish originated as well as tamilnadu and karnataka where muslim communities were present.Another theory claims that the dish was prepared in india before the first mughal emporer babur conquered india. Ingredients for biriyani vary according to the region and the type of meat and vegetables used. Meat is the prime ingredient with rice. As is common in dishes of the indian subcontinent. vegetables are sometimes also used when preparing biriyani. Corn may be used depending on the season and availability navratan biriyani tends to use sweeter
+</p>
+
+<ul>
+    <li><h1>TYPES OF BIRIYANI</h1></li>
+ <li>kolkata biriyani</li>
+ <li>Malabar biriyani</li>
+ <li>Ambur biriyani</li>
+<li>Hydrabad biriyani</li>
+<li>Thalassery biriyani</li>
+
+
+
+
+
+</ul>
+</div>
+  
+    
+}
+
